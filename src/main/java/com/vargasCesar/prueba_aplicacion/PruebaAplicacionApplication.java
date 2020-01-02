817d82695b00bf4be0a1c6bbb24cc63947f41335
César Vargas Escorcia <thecesarvargas@gmail.com>
2020-01-02 15:41:14 -0600

    Primer commit

---
+++ src/main/java/com/vargasCesar/prueba_aplicacion/PruebaAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.vargasCesar.prueba_aplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAplicacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vargasCesar/prueba_aplicacion/PruebaAplicacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vargasCesar.prueba_aplicacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaAplicacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
